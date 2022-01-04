7533353fa459e8208011ad62569e85550240c7e0
SHAO-CHUAN-LIN <97021002+SHAO-CHUAN-LIN@users.noreply.github.com>
2022-01-04 19:24:39 +0800

    Delete webduino-module-web-bluetooth-gh-pages/blockly directory

--- webduino-module-web-bluetooth-gh-pages/blockly/blocks.js
+++
@@ -1,9 +0,0 @@
-Blockly.Blocks['enable_web_bluetooth'] = {
-  init: function () {
-    this.appendDummyInput()
-      .appendField("Enable Web Bluetooth");
-    this.setColour(230);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
